a9e366602d314bea095d43a4f6b37317a673fe65
diliprahul <rahuldilip231@gmail.com>
2026-01-06 14:37:18 +0530

    Disable datasource auto-configuration for deployment

--- trip-planner-backend/src/main/java/com/tripplanner/backend/TripPlannerBackendApplication.java
+++ trip-planner-backend/src/main/java/com/tripplanner/backend/TripPlannerBackendApplication.java
@@ -2,12 +2,14 @@ package com.tripplanner.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = { DataSourceAutoConfiguration.class }
+)
 public class TripPlannerBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TripPlannerBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TripPlannerBackendApplication.class, args);
+    }
 }
